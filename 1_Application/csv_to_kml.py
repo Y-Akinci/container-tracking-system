9d390438e7dc08e3c9581d3d7843f2d484e844e8
Y-Akinci <yaren-can@hotmail.ch>
2026-03-23 12:36:10 +0100

    marker added

--- 1_Application/csv_to_kml.py
+++ 1_Application/csv_to_kml.py
@@ -12,8 +12,14 @@ kml = simplekml.Kml()
 
 inputfile = csv.reader(open(csv_path, "r"))
 for row in inputfile:
-    pnt = kml.newpoint(name="olten_brugg", coords=[(row[1], row[2])])
+    pnt = kml.newpoint(name="olten_brugg", coords=[(row[2], row[1])])
     pnt.description = f"DateTime: {row[0]}, Temperatur: {row[3]}, Luftfeuchtigkeit: {row[4]}, "
-    
+    if float(row[3]) >= 25:
+        pnt.style.iconstyle.color = simplekml.Color.orange
+    elif float(row[3]) >= 25 and float(row[4]) >= 75:
+        pnt.style.iconstyle.color = simplekml.Color.orange
+    else:
+        pnt.style.iconstyle.color = simplekml.Color.blue
+
 kml.save(str(script_dir / "olten-brugg.kml"))
 
