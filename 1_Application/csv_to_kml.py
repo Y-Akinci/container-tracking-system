3ae1985304c4cb059c636f92d7334b4d26924bd7
haris salii <harissalii745@gmail.com>
2026-03-23 21:07:29 +0100

    code implemetaion for better rendering

--- 1_Application/csv_to_kml.py
+++ 1_Application/csv_to_kml.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import csv
+from matplotlib import colors
 import simplekml
 
 # Zugriff auf die CSV-Datei im selben Verzeichnis wie das Skript
@@ -11,15 +12,40 @@ kml = simplekml.Kml()
 
 
 inputfile = csv.reader(open(csv_path, "r"))
+
+segments = []
+current_color = None
+current_coords = []
+
 for row in inputfile:
     pnt = kml.newpoint(name="olten_brugg", coords=[(row[2], row[1])])
     pnt.description = f"DateTime: {row[0]}, Temperatur: {row[3]}, Luftfeuchtigkeit: {row[4]}, "
-    if float(row[3]) >= 25:
+    pnt.style.iconstyle.scale = 0
+
+    if float(row[3]) >= 25 and float(row[4]) >= 80:
+        pnt.style.iconstyle.color = simplekml.Color.red
+    elif float(row[3]) >= 25:
         pnt.style.iconstyle.color = simplekml.Color.orange
-    elif float(row[3]) >= 25 and float(row[4]) >= 75:
-        pnt.style.iconstyle.color = simplekml.Color.orange
+    elif float(row[4]) >= 80:
+        pnt.style.iconstyle.color = simplekml.Color.yellow
     else:
         pnt.style.iconstyle.color = simplekml.Color.blue
+
+
+    if pnt.style.iconstyle.color != current_color:
+        if current_coords:
+            segments.append((current_color, current_coords))
+            current_coords = [current_coords[-1]]
+        current_color = pnt.style.iconstyle.color
+ 
+    current_coords.append((float(row[2]), float(row[1])))
+ 
+segments.append((current_color, current_coords))
+ 
+for i, (color, coords) in enumerate(segments):
+    line = kml.newlinestring(name=f"Route_{i}", coords=coords)
+    line.style.linestyle.width = 3
+    line.style.linestyle.color = color
 
 kml.save(str(script_dir / "olten-brugg.kml"))
 
